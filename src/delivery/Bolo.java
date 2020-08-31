5b87dbbd4065c263c7a8ccc30045714d86ba2a60
goncalves-ac <53871994+goncalves-ac@users.noreply.github.com>
2020-08-31 15:59:34 -0300

    Calcula tempo

--- src/delivery/Bolo.java
+++ src/delivery/Bolo.java
@@ -4,7 +4,7 @@ public class Bolo extends Lanche {
 	
 	@Override
 	public int calculaTempo (int distancia) {
-		return super.calculaTempo(distancia + 10);
+		return super.calculaTempo(distancia) + 10;
 	}
 	
 }
